1fd626b1c11e2407f8abc4008617abbc9cc5c7ed
NikFilias <nikfil@DESKTOP-M6KPVL2.localdomain>
2020-11-19 21:48:13 +0200

    Kafka Producer requirements added

--- KafkaProducer/kafka-producer.py
+++ KafkaProducer/kafka-producer.py
@@ -1,9 +1,16 @@
 from kafka import KafkaProducer
 from json import dumps
+import requests
 
 producer = KafkaProducer(
    value_serializer=lambda m: dumps(m).encode('utf-8'), 
    bootstrap_servers=['kafka:9092'])
 
-producer.send("test", value={"hello": "producer"})
+resp = requests.get('http://www.randomnumberapi.com/api/v1.0/random?min=100&max=1000&count=5')
+if resp.status_code != 200:
+    # This means something went wrong.
+    raise ApiError('GET Random Numbers {}'.format(resp.status_code))
+for random_number in resp.json():
+    producer.send("test", value={"Height": random_number})
+
 producer.flush()
